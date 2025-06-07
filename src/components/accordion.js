fb02f2fd58724ece863fe26deec8cc2f03f00093
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2025-06-07 23:13:41 +0530

    added : fixed label for length and width

--- src/components/accordion.js
+++ src/components/accordion.js
@@ -52,8 +52,8 @@ export default function ControlledAccordions() {
         <AccordionDetails>
           <Divider />
           <div style={{ display: "flex" }}>
-            <CustomizedSelect label="Width" data={widthOptions} />
-            <CustomizedSelect label="Length" data={lengthOptions} />
+            <CustomizedSelect label="Floor width" data={widthOptions} />
+            <CustomizedSelect label="Floor length" data={lengthOptions} />
           </div>
           <div style={{ margin: "8px", marginBottom: "20px" }}>
             <Typography
@@ -103,73 +103,6 @@ export default function ControlledAccordions() {
         </AccordionDetails>
         <Divider />
       </Accordion>
-      {/* <Accordion
-        expanded={expanded === "panel2"}
-        onChange={handleChange("panel2")}
-      >
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2bh-content"
-          id="panel2bh-header"
-        >
-          <Typography component="span" sx={{ width: "33%", flexShrink: 0 }}>
-            Users
-          </Typography>
-          <Typography component="span" sx={{ color: "text.secondary" }}>
-            You are currently not an owner
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Donec placerat, lectus sed mattis semper, neque lectus feugiat
-            lectus, varius pulvinar diam eros in elit. Pellentesque convallis
-            laoreet laoreet.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion
-        expanded={expanded === "panel3"}
-        onChange={handleChange("panel3")}
-      >
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel3bh-content"
-          id="panel3bh-header"
-        >
-          <Typography component="span" sx={{ width: "33%", flexShrink: 0 }}>
-            Advanced settings
-          </Typography>
-          <Typography component="span" sx={{ color: "text.secondary" }}>
-            Filtering has been entirely disabled for whole web server
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer
-            sit amet egestas eros, vitae egestas augue. Duis vel est augue.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion
-        expanded={expanded === "panel4"}
-        onChange={handleChange("panel4")}
-      >
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel4bh-content"
-          id="panel4bh-header"
-        >
-          <Typography component="span" sx={{ width: "33%", flexShrink: 0 }}>
-            Personal data
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer
-            sit amet egestas eros, vitae egestas augue. Duis vel est augue.
-          </Typography>
-        </AccordionDetails>
-      </Accordion> */}
     </div>
   );
 }
